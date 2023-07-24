78922121c422437d01e2af9b9e3ef9fb3d65c178
ljeongseok <ljeongseok@gmarket.com>
2023-07-25 07:53:03 +0900

    실행 flow 변경.

--- app/src/main/java/com/laonda/kckitsamplekt/MainActivity.kt
+++ app/src/main/java/com/laonda/kckitsamplekt/MainActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.util.JsonReader
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -14,40 +15,33 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.laonda.kckit.KCConfig
 import com.laonda.kckit.KCSessionManager
+import com.laonda.kckit.KCSessionManager.KCManagerListener
 import com.laonda.kckit.KCType
 import com.laonda.kckit.KCUser
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.io.InputStreamReader
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
-    private val TAG = "MainActivity"
-
-    private val TEST_APP_ID = "Conference3"
-    private val TEST_SECRET_KEY = "Conference3_secret"
-    private val TEST_GET_TOKEN_URL = "https://ticket-api.cod2f.dev/v1/client/access-token"
-    private val TEST_GET_SESSION_URL = "https://ticket-api.cod2f.dev/v1/client/session-node"
     private val TEST_ROOM_ID = "shawn"
-
-    private var isPublish = true
-    private var is1stPublish = false
+    private var isPublish = false
+    private var isJoined = false
     private var isSpeaker = false
-
-    private val config: KCConfig = KCConfig.getInstance()
-
-    private val kcSessionManager: KCSessionManager = KCSessionManager.getInstance()
-
+    private val config = KCConfig.getInstance()
+    private val kcSessionManager = KCSessionManager.getInstance()
     private val renders: MutableMap<String, View> = HashMap()
-
-    private val listener: KCSessionManager.KCManagerListener = object : KCSessionManager.KCManagerListener {
+    private val listener: KCManagerListener = object : KCManagerListener {
         override fun onAddStream(
             manager: KCSessionManager,
-            roomId: String?,
+            roomId: String,
             userId: String,
             mediaType: Int
         ) {
             println("onAddStream")
-            CoroutineScope(Dispatchers.Main).launch {
+            runOnUiThread {
                 if (mediaType > KCType.KCMediaType.KC_AUDIO) {
                     val view = manager.getRender(TEST_ROOM_ID, userId) as View
                     renders[userId] = view
@@ -56,20 +50,20 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        override fun onRemoveStream(manager: KCSessionManager?, roomId: String?, userId: String?) {
+        override fun onRemoveStream(manager: KCSessionManager, roomId: String, userId: String) {
             println("didLeaveRoom")
-            userId?.let { _userId ->
-                CoroutineScope(Dispatchers.Main).launch {
-                    removeRender(_userId)
-                }
-            }
+            runOnUiThread { removeRender(userId) }
         }
 
-        override fun didOccurError(manager: KCSessionManager?, errorCode: Int, errorMessage: String) {
+        override fun didOccurError(
+            manager: KCSessionManager,
+            errorCode: Int,
+            errorMessage: String
+        ) {
             println("didOccurError$errorCode $errorMessage")
             if (errorCode == KCType.KCError.KC_ERROR_NEED_REFRESH_PUB) {
-                CoroutineScope(Dispatchers.Main).launch {
-                    val mainLayout = findViewById(R.id.scrollLinear) as LinearLayout
+                runOnUiThread {
+                    val mainLayout = findViewById<View>(R.id.scrollLinear) as LinearLayout
                     mainLayout.removeView(renders[config.user])
                     renders.remove(config.user)
                     alignRender()
@@ -80,27 +74,23 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        override fun didRecvRoomInfo(manager: KCSessionManager, roomId: String, users: ArrayList<KCUser?>) {
-            CoroutineScope(Dispatchers.Main).launch {
+        override fun didRecvRoomInfo(
+            manager: KCSessionManager,
+            roomId: String,
+            users: ArrayList<KCUser>
+        ) {
+            runOnUiThread {
                 val arPresenter = ArrayList<String>()
                 users.forEach { user ->
-                    user?.let { _user ->
-                        Log.d(TAG, "didRecvRoomInfo] room:" + roomId + "user:" + _user.userId + "userType:" + _user.userType + "stMic:" + _user.stMic + "stCam:" + _user.stCam + "stScreen:" + _user.stScreen + "stData:" + _user.stData)
-                        if (_user.userId.equals(config.user) && user.userType === KCType.KCUserType.KC_USER_VIEWER) {
-                            if (is1stPublish == false) {
-                                Log.d(TAG, "sendMedia============================")
-                                manager.publish(TEST_ROOM_ID, KCType.KCMediaType.KC_AUDIO_VIDEO)
-                                Log.d(TAG, "startPreview============================")
-                                manager.startPreview(TEST_ROOM_ID)
-                                is1stPublish = true
-                            }
+                    Log.d(TAG,  "didRecvRoomInfo] room:" + roomId + "user:" + user.userId + "userType:" + user.userType + "stMic:" + user.stMic + "stCam:" + user.stCam + "stScreen:" + user.stScreen + "stData:" + user.stData)
+
+                    if (user.userId == config.user && user.userType == KCType.KCUserType.KC_USER_VIEWER) {
+                        if (isJoined == false) {
+                            isJoined = true
                         }
-                        if (user.userType === KCType.KCUserType.KC_USER_PRESENTER && !_user.userId.equals(
-                                config.user
-                            )
-                        ) {
-                            arPresenter.add(_user.userId)
-                        }
+                    }
+                    if (user.userType == KCType.KCUserType.KC_USER_PRESENTER && user.userId != config.user) {
+                        arPresenter.add(user.userId)
                     }
                 }
 
@@ -110,20 +100,22 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        override fun didChangeAudioLevel(manager: KCSessionManager?, userId: String?, audioLevel: Double) {
+        override fun didChangeAudioLevel(
+            manager: KCSessionManager,
+            userId: String,
+            audioLevel: Double
+        ) {
 //            System.out.println("didChangeAudioLevel:"+userId+" "+audioLevel);
         }
 
         override fun didChangeVideoSize(
-            manager: KCSessionManager?,
+            manager: KCSessionManager,
             userId: String,
             width: Int,
             height: Int
         ) {
             println("didChangeVideoSize:$userId $width $height")
-            CoroutineScope(Dispatchers.Main).launch {
-                alignRender()
-            }
+            runOnUiThread { alignRender() }
         }
     }
 
@@ -131,10 +123,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        checkPermission()
+        setPermission()
 
         config.countryCode = applicationContext.resources.configuration.locale.country
-        config.appID = "Meet3"
+        config.appID = "432e409445f9ab18a74d5c604c97addf"
         config.apiToken = "klang"
         config.sessionNode = "ws://3.37.135.207:7780/ws"
         config.user = "shawnAndroid"
@@ -143,7 +135,6 @@ class MainActivity : AppCompatActivity() {
         config.captureHeight = 480
         config.captureFps = 15
         config.isFrontCamera = true
-
         config.enableHWCodec = true
         config.enableAEC = true
         config.enableAGC = true
@@ -152,28 +143,19 @@ class MainActivity : AppCompatActivity() {
         config.maxBitrate = 500
 
 //        getServiceInfo();
-
-
-//        getServiceInfo();
         kcSessionManager.initWithConfig(config, listener)
-
         val onClickListener =
             View.OnClickListener { view ->
                 when (view.id) {
-                    R.id.btnJoin -> kcSessionManager.joinRoomByToken(
-                        config.apiToken,
-                        TEST_ROOM_ID,
-                        config.user,
-                        KCType.KCMediaType.KC_AUDIO_VIDEO
-                    )
+                    R.id.btnJoin -> getToken()
                     R.id.btnLeave -> {
                         kcSessionManager.stopPreview(TEST_ROOM_ID)
                         kcSessionManager.leaveRoom(TEST_ROOM_ID)
-                        is1stPublish = false
+                        isJoined = false
                         clearRender()
                     }
                     R.id.btnUnPub -> if (isPublish == true) {
-                        val mainLayout = findViewById(R.id.scrollLinear) as LinearLayout
+                        val mainLayout = findViewById<View>(R.id.scrollLinear) as LinearLayout
                         mainLayout.removeView(renders[config.user])
                         renders.remove(config.user)
                         alignRender()
@@ -188,9 +170,11 @@ class MainActivity : AppCompatActivity() {
                         renders[config.user] = localrender
                         alignRender()
                         kcSessionManager.startPreview(TEST_ROOM_ID)
+                        kcSessionManager.resumeRecording(TEST_ROOM_ID, KCType.KCMediaType.KC_AUDIO)
+                        kcSessionManager.resumeRecording(TEST_ROOM_ID, KCType.KCMediaType.KC_VIDEO)
                     }
                     R.id.btnUnsubscribe -> {
-                        val arPresenter = java.util.ArrayList<String>()
+                        val arPresenter = ArrayList<String>()
                         for (key in renders.keys) {
                             if (key != config.user) arPresenter.add(key)
                         }
@@ -206,22 +190,21 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
             }
-
-        val btnJoin = findViewById(R.id.btnJoin) as Button
+        val btnJoin = findViewById<View>(R.id.btnJoin) as Button
         btnJoin.setOnClickListener(onClickListener)
-        val btnLeave = findViewById(R.id.btnLeave) as Button
+        val btnLeave = findViewById<View>(R.id.btnLeave) as Button
         btnLeave.setOnClickListener(onClickListener)
-        val btnUnsub = findViewById(R.id.btnUnsubscribe) as Button
+        val btnUnsub = findViewById<View>(R.id.btnUnsubscribe) as Button
         btnUnsub.setOnClickListener(onClickListener)
-        val btnPub = findViewById(R.id.btnUnPub) as Button
+        val btnPub = findViewById<View>(R.id.btnUnPub) as Button
         btnPub.setOnClickListener(onClickListener)
-        val btnCamera = findViewById(R.id.btnCamera) as Button
+        val btnCamera = findViewById<View>(R.id.btnCamera) as Button
         btnCamera.setOnClickListener(onClickListener)
-        val btnSpeaker = findViewById(R.id.btnSpeaker) as Button
+        val btnSpeaker = findViewById<View>(R.id.btnSpeaker) as Button
         btnSpeaker.setOnClickListener(onClickListener)
     }
 
-    fun checkPermission() {
+    fun setPermission() {
         val deniedPermissions = arrayListOf<String>()
 
         val permissions: Array<String> = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) arrayOf(
@@ -259,7 +242,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun alignRender() {
-        val mainLayout = findViewById(R.id.scrollLinear) as LinearLayout
+        val mainLayout = findViewById<View>(R.id.scrollLinear) as LinearLayout
         for (key in renders.keys) {
             renders[key]!!.layoutParams = LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
@@ -273,17 +256,74 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun removeRender(userId: String) {
-        val mainLayout = findViewById(R.id.scrollLinear) as LinearLayout
+        val mainLayout = findViewById<View>(R.id.scrollLinear) as LinearLayout
         mainLayout.removeView(renders[userId])
         renders.remove(userId)
         alignRender()
     }
 
     private fun clearRender() {
-        val mainLayout = findViewById(R.id.scrollLinear) as LinearLayout
+        val mainLayout = findViewById<View>(R.id.scrollLinear) as LinearLayout
         for (key in renders.keys) {
             mainLayout.removeView(renders[key])
         }
         renders.clear()
+    }
+
+    private fun getToken() {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val urlstr =
+                    "https://jwt.klang.network:8008/rtc-token?room_id=" + TEST_ROOM_ID + "&user_id=" + config.user
+                val url = URL(urlstr)
+                val conn = url.openConnection() as HttpsURLConnection
+                conn.requestMethod = "POST"
+                conn.setRequestProperty("Content-Type", "application/json")
+                conn.setRequestProperty("Accept", "application/json")
+                conn.doOutput = true
+                conn.doInput = true
+
+                //                    JSONObject json = new JSONObject();
+                //                    json.put("app_id", TEST_APP_ID);
+                //                    json.put("secret_key", TEST_SECRET_KEY);
+                //                    json.put("room_id", TEST_ROOM_ID);
+                //                    json.put("login_id", config.user);
+                //
+                //                    OutputStream outputStream;
+                //                    outputStream = conn.getOutputStream();
+                //                    outputStream.write(json.toString().getBytes());
+                //                    outputStream.flush();
+
+                // 실제 서버로 Request 요청 하는 부분 (응답 코드를 받음, 200은 성공, 나머지 에러)
+                val response = conn.responseCode
+                val responseMessage = conn.responseMessage
+                val responseBody = conn.inputStream
+                val responseBodyReader = InputStreamReader(responseBody, "UTF-8")
+                val jsonReader = JsonReader(responseBodyReader)
+                jsonReader.beginObject() // Start processing the JSON object
+                while (jsonReader.hasNext()) { // Loop through all keys
+                    val key = jsonReader.nextName() // Fetch the next key
+                    if (key == "jwt") { // Check if desired key
+                        config.apiToken = jsonReader.nextString()
+                    } else {
+                        jsonReader.skipValue() // Skip values of other keys
+                    }
+                }
+                conn.disconnect()
+                kcSessionManager.initWithConfig(config, listener)
+                kcSessionManager.joinRoomByToken(
+                    config.apiToken,
+                    TEST_ROOM_ID,
+                    config.user,
+                    KCType.KCMediaType.KC_AUDIO_VIDEO
+                )
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
